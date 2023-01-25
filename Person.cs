c4da116c01f38bc37ad774d3f241808610559791
Junyoung Kim <jkim5929@gmail.com>
2023-01-25 16:07:46 -0500

    Made person class

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pair_Project
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string _name, int _age)
+        {
+            Name = _name;
+            Age = _age;
+        }
+    }
+}
