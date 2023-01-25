f692b46c8d372693283ff60525c6453cc2492194
carissa-c <122126939+carissa-c@users.noreply.github.com>
2023-01-25 15:41:16 -0600

    Name List Added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Pair_Project;
+List<Person> duck = new List<Person>()
+{
+    new Person("Junyoung", 24),
+    new Person("Carissa", 28),
+    new Person("Ethan", 28),
+};
